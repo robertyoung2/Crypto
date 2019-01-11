4fbe2ffbc0d2111a5ee0c6e9791ca777c463f10e
Robert Young <robert.young.ie@gmail.com>
2019-01-11 14:06:16 +0000

    Part 1 complete - normalise text

--- src/Crypto.java
+++ src/Crypto.java
@@ -1,2 +1,36 @@
+import java.util.*;
+
 public class Crypto {
+    public static void main(String[] args) {
+        // take in a string to be encrypted
+        normaliseText("Hello world    ggggg(. , : ; ’ ” ! ? ( ) )");
+    }
+    public static String normaliseText(String s) {
+
+        // remove all spaces and punctuation
+        s = s.replaceAll("\\s|[^a-zA-Z0-9]","");
+
+        // make all upper case
+        s = s.toUpperCase();
+
+        // return the result
+        return s;
+
+    }
+//    public static String obify() {
+//        return obifiedText;
+//
+//    }
+//    public static String ceasarify() {
+//        return ceasarifiedText;
+//
+//    }
+//    public static String groupify() {
+//        return groupifiedText;
+//    }
+//
+//    public static String encryptString() {
+//
+//    }
+
 }
