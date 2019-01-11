21614f9d8b70b05c9d0d3581dc2e5de4e0e9f9f4
Robert Young <robert.young.ie@gmail.com>
2019-01-11 14:38:23 +0000

    Obify function complete

--- src/Crypto.java
+++ src/Crypto.java
@@ -4,6 +4,7 @@ public class Crypto {
     public static void main(String[] args) {
         // take in a string to be encrypted
         normaliseText("Hello world    ggggg(. , : ; ’ ” ! ? ( ) )");
+        obify("FIRE");
     }
     public static String normaliseText(String s) {
 
@@ -17,14 +18,47 @@ public class Crypto {
         return s;
 
     }
-//    public static String obify() {
-//        return obifiedText;
+    public static String obify(String s) {
+
+        s = s.replaceAll("(A|E|I|O|U|Y)", "OB$1");
+        return s;
+
+    }
+//    public static String ceasarify(String s, int key) {
 //
-//    }
-//    public static String ceasarify() {
+//
+//
+//
+//
 //        return ceasarifiedText;
 //
 //    }
+//
+//    // Method to shift alphabet by given shift value (positive or negative)
+//    public static String shiftAlphabet(int shift) {
+//
+//        int start = 0;
+//        if (shift < 0) {
+//            start = (int) 'Z' + shift + 1;
+//        } else {
+//            start = 'A' + shift;
+//        }
+//
+//        String result = "";
+//        char currChar = (char) start;
+//        for(; currChar <= 'Z'; ++currChar) {
+//            result = result + currChar;
+//        }
+//
+//        if(result.length() < 26) {
+//            for(currChar = 'A'; result.length() < 26; ++currChar) {
+//                result = result + currChar;
+//            }
+//        }
+//        return result;
+//    }
+
+
 //    public static String groupify() {
 //        return groupifiedText;
 //    }
