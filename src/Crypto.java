9037872f538a9b2835b330f180660508b569f68b
Robert Young <robert.young.ie@gmail.com>
2019-01-11 15:31:58 +0000

    Part 2 - Caesar Cipher implemented

--- src/Crypto.java
+++ src/Crypto.java
@@ -5,6 +5,7 @@ public class Crypto {
         // take in a string to be encrypted
         normaliseText("Hello world    ggggg(. , : ; ’ ” ! ? ( ) )");
         obify("FIRE");
+        ceasarify("ILIKEZOOS", 1);
     }
     public static String normaliseText(String s) {
 
@@ -24,39 +25,45 @@ public class Crypto {
         return s;
 
     }
-//    public static String ceasarify(String s, int key) {
-//
-//
-//
-//
-//
-//        return ceasarifiedText;
-//
-//    }
-//
-//    // Method to shift alphabet by given shift value (positive or negative)
-//    public static String shiftAlphabet(int shift) {
-//
-//        int start = 0;
-//        if (shift < 0) {
-//            start = (int) 'Z' + shift + 1;
-//        } else {
-//            start = 'A' + shift;
-//        }
-//
-//        String result = "";
-//        char currChar = (char) start;
-//        for(; currChar <= 'Z'; ++currChar) {
-//            result = result + currChar;
-//        }
-//
-//        if(result.length() < 26) {
-//            for(currChar = 'A'; result.length() < 26; ++currChar) {
-//                result = result + currChar;
-//            }
-//        }
-//        return result;
-//    }
+    public static String ceasarify(String s, int key) {
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String shifted_alphabet = shiftAlphabet(key);
+        String shifted_string = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char letter = s.charAt(i);
+            int index_alpha = alphabet.indexOf(String.valueOf(letter));
+            shifted_string += shifted_alphabet.charAt(index_alpha);
+        }
+
+        System.out.println(shifted_string);
+        return shifted_string;
+    }
+
+    // Method to shift alphabet by given shift value (positive or negative)
+    public static String shiftAlphabet(int shift) {
+
+        int start = 0;
+        if (shift < 0) {
+            start = (int) 'Z' + shift + 1;
+        } else {
+            start = 'A' + shift;
+        }
+
+        String result = "";
+        char currChar = (char) start;
+        for(; currChar <= 'Z'; ++currChar) {
+            result = result + currChar;
+        }
+
+        if(result.length() < 26) {
+            for(currChar = 'A'; result.length() < 26; ++currChar) {
+                result = result + currChar;
+            }
+        }
+        return result;
+    }
 
 
 //    public static String groupify() {
